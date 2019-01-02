d077a8a73108e4ac760bbfd8fc37d0e543344119
mmzzl1 <truongtuhuy2@gmail.com>
2019-01-02 21:52:39 +0700

    Update files

---
+++ assets/js/product_list.js
@@ -0,0 +1,11 @@
+var productList = [
+	{
+		id: 1,
+		name: '',
+		price: 0,
+		img: '',
+		rate: 0, // Danh gia -> Star Num
+		//category: '', // fashion, accessory, mobile,...
+		//order_status: '',
+	},
+];
